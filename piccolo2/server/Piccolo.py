9a47a530c5878457fc651470e637a19f68ecae3d
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2016-03-02 12:54:30 +0000

    add methods to get/set current time

--- piccolo2/server/Piccolo.py
+++ piccolo2/server/Piccolo.py
@@ -8,11 +8,14 @@ __all__ = ['Piccolo']
 from PiccoloInstrument import PiccoloInstrument
 import socket
 import psutil
+import subprocess
+import datetime
 
 class Piccolo(PiccoloInstrument):
     """piccolo server instrument
 
     the piccolo server itself is treated as an instrument"""
+
     def info(self):
         """get info
 
@@ -24,6 +27,24 @@ class Piccolo(PiccoloInstrument):
                 'virtual_memory': dict(psutil.virtmem_usage()._asdict())}
         return info
 
+    def getClock(self):
+        """get the current date and time
+
+        :returns: isoformat date and time string"""
+        return datetime.datetime.now().isoformat()
+
+    def setClock(self,clock=None):
+        """set the current date and time
+
+        :param clock: isoformat date and time string used to set the time"""
+        if clock!=None:
+            cmdPipe = subprocess.Popen(['date','-s',clock],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            if cmdPipe.wait()!=0:
+                raise OSError, 'setting date to \'{}\': {}'.format(clock,cmdPipe.stderr.read())
+            return cmdPipe.stdout.read()
+        return ''
+        
+
 if __name__ == '__main__':
     from piccoloLogging import *
 
@@ -32,3 +53,4 @@ if __name__ == '__main__':
     print p.ping()
     print p.status()
     print p.info()
+    print p.getClock()
