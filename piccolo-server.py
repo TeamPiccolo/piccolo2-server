2e88b7dea66dc5a125912298e32844f326adcdf5
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2016-03-09 17:21:03 +0000

    rearrange initialisation

--- piccolo-server.py
+++ piccolo-server.py
@@ -2,6 +2,8 @@ import piccolo_server as piccolo
 import cherrypy
 import argparse
 import urlparse
+import sys
+import logging
 
 if __name__ == '__main__':
     serverCfg = piccolo.PiccoloServerConfig()
@@ -9,6 +11,8 @@ if __name__ == '__main__':
     piccolo.piccoloLogging(logfile=serverCfg.cfg['logging']['logfile'],
                            debug=serverCfg.cfg['logging']['debug'])
 
+    log = logging.getLogger("piccolo.server")
+    
     # create data directory
     pData = piccolo.PiccoloDataDir(serverCfg.cfg['datadir']['datadir'],
                                    device=serverCfg.cfg['datadir']['device'],
@@ -27,17 +31,34 @@ if __name__ == '__main__':
     pd.registerComponent(pc)
 
     # initialise the shutters
-    for cname in piccoloCfg.cfg['channels']:
-        channel = piccolo.PiccoloShutter(cname,
-                                         channel=piccoloCfg.cfg['channels'][cname]['shutter'],
-                                         reverse=piccoloCfg.cfg['channels'][cname]['reverse'],
-                                         fibreDiameter=piccoloCfg.cfg['channels'][cname]['fibreDiameter'])
-        pd.registerComponent(channel)
-
+    ok=True
+    for c in ['upwelling','downwelling']:
+        if c not in piccoloCfg.cfg['channels']:
+            log.error('{} shutter not defined'.format(c))
+            ok=False
+    if not ok:
+        sys.exit(1)
+    cname = 'upwelling'
+    upwellingShutter = piccolo.PiccoloShutter(
+        cname,
+        channel=piccoloCfg.cfg['channels'][cname]['shutter'],
+        reverse=piccoloCfg.cfg['channels'][cname]['reverse'],
+        fibreDiameter=piccoloCfg.cfg['channels'][cname]['fibreDiameter'])
+    pd.registerComponent(upwellingShutter)
+    cname = 'downwelling'
+    downwellingShutter = piccolo.PiccoloShutter(
+        cname,
+        channel=piccoloCfg.cfg['channels'][cname]['shutter'],
+        reverse=piccoloCfg.cfg['channels'][cname]['reverse'],
+        fibreDiameter=piccoloCfg.cfg['channels'][cname]['fibreDiameter'])
+    pd.registerComponent(downwellingShutter)
+    
     # initialise the spectrometers
+    spectrometers = []
     for sname in piccoloCfg.cfg['spectrometers']:
-        spectrometer = piccolo.PiccoloSpectrometer(sname)
-        pd.registerComponent(spectrometer)
+        spectrometers.append(piccolo.PiccoloSpectrometer(sname))
+    for s in spectrometers:
+        pd.registerComponent(s)
 
     pController = piccolo.PiccoloControllerCherryPy()
 
